d0ff4483a45a5544ae3415b34a4fbd06d93d1094
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-08-21 00:26:55 -0700

    runtime working, but no iframe, require dont work

--- example.notebook.js
+++ example.notebook.js
@@ -8,4 +8,21 @@ md`Hows it going boi`
   }
 }
 
-html`<div>yay`
+html`<div style="height: 200px; background-image: linear-gradient(to right, blue, yellow);">yay`
+
+
+function () {
+  return 'yee haw'
+}
+
+class {
+  uhOh(){}
+  yay(){}
+}
+
+200
+
+html`<img src="https://images.dog.ceo/breeds/dhole/n02115913_4117.jpg">`
+
+
+d3 = require('d3')
