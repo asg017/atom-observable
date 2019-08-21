84d1ae7607a0072306d006c0362b261e66d50af7
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-08-20 19:09:23 -0700

    initial

---
+++ spec/atom-observable-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomObservableView from '../lib/atom-observable-view';
+
+describe('AtomObservableView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
